ed5a30995335f3e570dac6ebfa5bea7860cd9dbb
Ariel Alsina <ariel.alsina2007@gmail.com>
2019-01-22 20:17:05 -0300

    Versão final do gerador de scalespace funcionando

--- scaleSpaceGenerator.m
+++ scaleSpaceGenerator.m
@@ -3,21 +3,19 @@ function scalespace = scaleSpaceGenerator(path_to_image)
     %the way we can to put all them in a single structure is in a structure
     %(lol).
   
-    
     image = imread(path_to_image);     %reads the corresponding image.
-    sub_image{1} = image;                 %this is a copy of image that we use to half the scale.
-    
+    sub_image = image;                 %sub-image that serves as a substitute to the original one.
+    scalespace = cell(1,5);            %creating the empty cell of the scalespace for speed.
+  
     for i=1:1:4                        %four are the number of octaves
         for j=1:1:5                    %here i store the images 5 of the corresponding octaves.
-            scalespace{i,j} = imgaussfilt(sub_image{i}, (2^(i)) * (sqrt(2)^(j-2)) );       %applying the gaussian blur
+            scalespace{i,j} = imgaussfilt(sub_image, (2^(i-1)) * (sqrt(2)^(j-2)) );       %applying the gaussian blur
         end
         
-        if i>=2
-            sub_image{i} = imresize(sub_image{i-1}, 0.5);       %creating the half sized images from the previous ones.
-        end
-        
+        sub_image = imresize(sub_image, 0.5);       %creating the half sized images from the previous ones.
+      
     end
     
-    clear sub_image;                   %deleting the used image to clear the unused memory.
+   % clear sub_image;                   %deleting the used image to clear the unused memory.
     
 end
