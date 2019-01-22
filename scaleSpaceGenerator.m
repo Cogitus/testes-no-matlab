a31c762195f8e7f1ea797048b9a21cf572ab5a52
Ariel Alsina <ariel.alsina2007@gmail.com>
2019-01-22 18:47:26 -0300

    Gerador de scalepaces finalizado.

--- scaleSpaceGenerator.m
+++ scaleSpaceGenerator.m
@@ -4,9 +4,21 @@ function scalespace = scaleSpaceGenerator(path_to_image)
     %(lol).
   
     
-    imagem = imread(path_to_image);     %reads the corresponding image.
+    image = imread(path_to_image);     %reads the corresponding image.
     
-    for i=1:1:5                         %here i store the images 5 of the octaves.
-        scalespace.first(i) = imagem
+    sub_image{1} = image;                 %this is a copy of image that we use to half the scale.
+    
+    for i=1:1:4                        %four are the number of octaves
+        for j=1:1:5                    %here i store the images 5 of the corresponding octaves.
+            scalespace{i,j} = imgaussfilt(sub_image{i}, (2^(i)) * (sqrt(2)^(j-2)) );       %applying the gaussian blur
+        end
+        
+        if i>=2
+            sub_image{i} = imresize(sub_image{i-1}, 0.5);       %creating the half sized images from the previous ones.
+        end
+        
     end
+    
+    clear sub_image;                   %deleting the used image to clear the unused memory.
+    
 end
