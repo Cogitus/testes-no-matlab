d820994d13b40d3d860c75c32c80dac55f41fb31
Ariel Alsina <ariel.alsina2007@gmail.com>
2019-01-22 16:16:07 -0300

    Presentation of the blurred images via montage.

--- borrandoAImagem.m
+++ borrandoAImagem.m
@@ -1,11 +1,13 @@
 imagem = imread('C:\Users\ariel\Pictures\Saved Pictures\4chan\dark\1541736761685.jpg');
 
-arrayimagem = [];
+%arrayimagem corresponds to the matrix-array that carries all the
+%procedural blurred images.
 
-for i=1:1:5
-    arrayimagem(i) = imgaussfilt(imagem,i);
+for i=1:1:6
+    cellArrayImagem{i} = imgaussfilt(imagem,i);
 end
 
+montage(cellArrayImagem);
 
 % the following syntax can be used to create multiple figure windows
 % that in the future will contain their corresponding images:
