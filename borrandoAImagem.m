209203da964b88534667e261173a4cab0e06fb6a
Ariel Alsina <ariel.alsina2007@gmail.com>
2019-01-22 16:09:30 -0300

    A more general aproach is being used in the presentation of the blurred images here using the function 'montage()'

--- borrandoAImagem.m
+++ borrandoAImagem.m
@@ -1,5 +1,16 @@
 imagem = imread('C:\Users\ariel\Pictures\Saved Pictures\4chan\dark\1541736761685.jpg');
 
+arrayimagem = [];
+
 for i=1:1:5
-    figure, imshow(imgaussfilt(imagem,i));
+    arrayimagem(i) = imgaussfilt(imagem,i);
 end
+
+
+% the following syntax can be used to create multiple figure windows
+% that in the future will contain their corresponding images:
+%             
+%             figure, imshow(image);
+%             
+%             
+%             NOTE: figure is a function by itself!
